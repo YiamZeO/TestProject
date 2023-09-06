7bcb3e05279a1086b2898a89af9cfa03fb6b942e
ZeO <102325825+YiamZeO@users.noreply.github.com>
2023-09-06 21:32:51 +0300

    _

--- usersDb/src/main/java/com/example/usersdb/configs/SecurityConfig.java
+++ usersDb/src/main/java/com/example/usersdb/configs/SecurityConfig.java
@@ -4,16 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -35,14 +31,8 @@ public class SecurityConfig {
     }
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/usersdb/users").hasRole("ADMIN")
-                        .anyRequest().authenticated())
-                .httpBasic(withDefaults())
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authorizeRequests()
+                .anyRequest().permitAll();
         return http.build();
     }
 }
