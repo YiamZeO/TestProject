f4000293f82b09f127509fb7c3d7d6e2b8c35368
ZeO <102325825+YiamZeO@users.noreply.github.com>
2023-09-06 21:50:18 +0300

    _

--- usersDb/src/main/java/com/example/usersdb/configs/SecurityConfig.java
+++ usersDb/src/main/java/com/example/usersdb/configs/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().permitAll();
+                .antMatchers();
         return http.build();
     }
 }
