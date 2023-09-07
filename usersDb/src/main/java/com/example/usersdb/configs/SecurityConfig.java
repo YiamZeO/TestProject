af8ba07eda45e20715531e9faf2500d6a3cdd6e7
ZeO <102325825+YiamZeO@users.noreply.github.com>
2023-09-07 11:01:17 +0300

    _

--- usersDb/src/main/java/com/example/usersdb/configs/SecurityConfig.java
+++ usersDb/src/main/java/com/example/usersdb/configs/SecurityConfig.java
@@ -2,14 +2,19 @@ package com.example.usersdb.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.http.MatcherType.ant;
 
 @Configuration
 @EnableWebSecurity
@@ -31,8 +36,14 @@ public class SecurityConfig {
     }
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers();
+        http.authorizeHttpRequests((authz)->authz
+                .requestMatchers(new AntPathRequestMatcher("/users")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/users/**")).hasRole("ADMIN")
+                        .requestMatchers(new AntPathRequestMatcher("usersdb/users")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("usersdb/users/**")).hasRole("ADMIN")
+                .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
--- usersDb/src/main/java/com/example/usersdb/entities/Group.java
+++ usersDb/src/main/java/com/example/usersdb/entities/Group.java
@@ -1,7 +1,7 @@
 package com.example.usersdb.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import javax.persistence.*;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
