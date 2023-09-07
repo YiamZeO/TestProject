f7eed4d0ea6d5fd8acf4a0c15b602da084004304
ZeO <102325825+YiamZeO@users.noreply.github.com>
2023-09-07 18:44:51 +0300

    _

--- usersDb/src/main/java/com/example/usersdb/configs/SecurityConfig.java
+++ usersDb/src/main/java/com/example/usersdb/configs/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.usersdb.configs;
 
+import com.example.usersdb.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
